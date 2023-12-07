2b785957a7e8308a889949ef56f4cae657f93b08
shaeel45 <beingshaeel@gmail.com>
2023-12-07 14:29:38 +0500

    working

---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import './Style.css';
+
+export function Footer(props){
+    return(
+        <>
+        <h1>{props.title}</h1>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import './Style.css';
+
+export function Header (props){
+    return(
+        <>
+        <p>{props.title}</p>
+        </>
+    )
+}
+
+export default Header;
